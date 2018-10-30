c9d8a980663b721053df5ca975d0330a24471f3f
MohammedAlnajeh <43445141+MohammedAlnajeh@users.noreply.github.com>
2018-10-30 14:36:21 -0400

    added stuff

--- FactorialDoWhileMo/FactorialDoWhileMo/FactorialDoWhileForm.Designer.cs
+++ FactorialDoWhileMo/FactorialDoWhileMo/FactorialDoWhileForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace FactorialDoWhileMo
 {
-    partial class Form1
+    partial class frmFactorialDoWhile
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblEnterNumber = new System.Windows.Forms.Label();
+            this.txt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblEnterNumber
+            // 
+            this.lblEnterNumber.AutoSize = true;
+            this.lblEnterNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnterNumber.Location = new System.Drawing.Point(52, 95);
+            this.lblEnterNumber.Name = "lblEnterNumber";
+            this.lblEnterNumber.Size = new System.Drawing.Size(206, 25);
+            this.lblEnterNumber.TabIndex = 0;
+            this.lblEnterNumber.Text = "Enter The Number :-";
+            // 
+            // txt
+            // 
+            this.txt.Location = new System.Drawing.Point(274, 100);
+            this.txt.Name = "txt";
+            this.txt.Size = new System.Drawing.Size(100, 20);
+            this.txt.TabIndex = 1;
+            // 
+            // frmFactorialDoWhile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(824, 446);
+            this.Controls.Add(this.txt);
+            this.Controls.Add(this.lblEnterNumber);
+            this.Name = "frmFactorialDoWhile";
+            this.Text = "Factorial Do While By Mohammed";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblEnterNumber;
+        private System.Windows.Forms.TextBox txt;
     }
 }
 
