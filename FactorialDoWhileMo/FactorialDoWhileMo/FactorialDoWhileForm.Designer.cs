9e68b8315f42a0ed380a43fc6f9d351731c1147b
MohammedAlnajeh <43445141+MohammedAlnajeh@users.noreply.github.com>
2018-10-31 11:03:56 -0400

    finished factorial do while loop

--- FactorialDoWhileMo/FactorialDoWhileMo/FactorialDoWhileForm.Designer.cs
+++ FactorialDoWhileMo/FactorialDoWhileMo/FactorialDoWhileForm.Designer.cs
@@ -29,33 +29,80 @@
         private void InitializeComponent()
         {
             this.lblEnterNumber = new System.Windows.Forms.Label();
-            this.txt = new System.Windows.Forms.TextBox();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblMultiply = new System.Windows.Forms.Label();
+            this.lstNumbers = new System.Windows.Forms.ListBox();
+            this.lblAnswer = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblEnterNumber
             // 
             this.lblEnterNumber.AutoSize = true;
             this.lblEnterNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblEnterNumber.Location = new System.Drawing.Point(52, 95);
+            this.lblEnterNumber.Location = new System.Drawing.Point(33, 63);
             this.lblEnterNumber.Name = "lblEnterNumber";
             this.lblEnterNumber.Size = new System.Drawing.Size(206, 25);
             this.lblEnterNumber.TabIndex = 0;
             this.lblEnterNumber.Text = "Enter The Number :-";
             // 
-            // txt
+            // txtNumber
             // 
-            this.txt.Location = new System.Drawing.Point(274, 100);
-            this.txt.Name = "txt";
-            this.txt.Size = new System.Drawing.Size(100, 20);
-            this.txt.TabIndex = 1;
+            this.txtNumber.Location = new System.Drawing.Point(258, 69);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtNumber.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(187, 130);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(80, 37);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblMultiply
+            // 
+            this.lblMultiply.AutoSize = true;
+            this.lblMultiply.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMultiply.Location = new System.Drawing.Point(51, 196);
+            this.lblMultiply.Name = "lblMultiply";
+            this.lblMultiply.Size = new System.Drawing.Size(342, 24);
+            this.lblMultiply.TabIndex = 3;
+            this.lblMultiply.Text = "Multiply the following numbers together:";
+            // 
+            // lstNumbers
+            // 
+            this.lstNumbers.FormattingEnabled = true;
+            this.lstNumbers.Location = new System.Drawing.Point(122, 232);
+            this.lstNumbers.Name = "lstNumbers";
+            this.lstNumbers.Size = new System.Drawing.Size(203, 186);
+            this.lstNumbers.TabIndex = 4;
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.Location = new System.Drawing.Point(84, 438);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(21, 24);
+            this.lblAnswer.TabIndex = 5;
+            this.lblAnswer.Text = "?";
             // 
             // frmFactorialDoWhile
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(824, 446);
-            this.Controls.Add(this.txt);
+            this.ClientSize = new System.Drawing.Size(457, 480);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.lstNumbers);
+            this.Controls.Add(this.lblMultiply);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtNumber);
             this.Controls.Add(this.lblEnterNumber);
             this.Name = "frmFactorialDoWhile";
             this.Text = "Factorial Do While By Mohammed";
@@ -67,7 +114,11 @@
         #endregion
 
         private System.Windows.Forms.Label lblEnterNumber;
-        private System.Windows.Forms.TextBox txt;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblMultiply;
+        private System.Windows.Forms.ListBox lstNumbers;
+        private System.Windows.Forms.Label lblAnswer;
     }
 }
 
--- FactorialDoWhileMo/FactorialDoWhileMo/FactorialDoWhileForm.cs
+++ FactorialDoWhileMo/FactorialDoWhileMo/FactorialDoWhileForm.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * Created by: Mohammed Alnajeh
+ * Created on: 2018-10-31
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #20 - Factorial Do While
+ * This program...
+*/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +23,43 @@ namespace FactorialDoWhileMo
         public frmFactorialDoWhile()
         {
             InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //declare local variables
+            Double factorialAnswer;
+            Double FactorialNumber;
+            int factorialCounter;
+
+            //clear the items from the listbox
+            this.lstNumbers.Items.Clear();
+
+            //initialize the final answer to 1
+            factorialAnswer = 1;
+
+            //get the number from the user 
+            FactorialNumber = Convert.ToDouble(this.txtNumber.Text);
+
+            //set the counter to 0 
+            factorialCounter = 0;
+
+            //multiply the counter by the next incremented number until it reaches the user's number 
+            do
+            {
+                //increment the counter by 1
+                factorialCounter = factorialCounter + 1;
+
+                //list the counter number in the listbox for the user to see
+                lstNumbers.Items.Add(factorialCounter);
+
+                //multiplay the counter by the answer
+                factorialAnswer = factorialAnswer * factorialCounter;
+
+            } while (factorialCounter < FactorialNumber);
+
+            //convert the factorialAnswer to a string and insert it into the label
+            this.lblAnswer.Text = this.txtNumber.Text + "! = " + Convert.ToString(factorialAnswer);
         }
     }
 }
